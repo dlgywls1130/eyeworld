345b4d51343fccf6ff7b2878d70e46a347fb03b3
dlgywls1130 <dlgywls1130@gmail.com>
2025-02-17 22:02:26 +0900

    first commit

---
+++ src/main/java/com/example/eyeworld/EyeworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.eyeworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyeworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eyeworld/EyeworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eyeworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyeworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
